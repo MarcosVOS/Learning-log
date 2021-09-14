3049139e210fa8bfe9d89e7bc176fa6a429da5c8
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2021-09-14 18:18:26 -0300

    add 404 error

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render,redirect,get_object_or_404
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
@@ -27,6 +28,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
+    topic = get_object_or_404(Topic,id=topic_id)
     topic = Topic.objects.get(id=topic_id)
     # Makse sure the topic belogns to the current user.
     check_topic_owner(request, topic)
